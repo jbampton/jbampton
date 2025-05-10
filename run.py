1d82bf31d3a5a8246c71b09858da9d646f699d53
John Bampton <jbampton@users.noreply.github.com>
2025-05-10 17:34:54 +1000

    Update run.py (#160)

--- run.py
+++ run.py
@@ -131,6 +131,8 @@ def build_readme():
     <br>  
     <a href="https://www.codewars.com/users/Beast">Nikyū 二級</a>
     <br>
+    <a href="#">Jun'ikkyū 準一級<br><img src="images/pre-1st-kyu.png" alt="Jun'ikkyū 準一級" title="Jun'ikkyū 準一級"></a>
+    <br>
   </p>
   <p>
     <img src="images/ImRuby.gif"
